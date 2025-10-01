0d9dd73c6110a4922f7854896e1aa1b950114d04
spscah <cah@stpaulsschool.org.uk>
2025-10-01 10:35:13 +0100

    Added the Sieve to the Iteration Answers

---
+++ Answers/Iteration.Answers/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Text;
+
+int limit = 1000;
+
+IList<int> primes = Sieve(limit);
+
+Console.WriteLine($"Primes under {limit}: {PrettyPrint(primes)}");
+
+static string PrettyPrint<T>(IList<T> items)
+{
+    StringBuilder sb = new StringBuilder();
+    sb.Append("[");
+    sb.Append(string.Join(", ", items));
+    sb.Append("]");
+    return sb.ToString();
+}
+
+
+static IList<int> Sieve(int n)
+{
+    List<int> candidates = Enumerable.Range(2, n - 1).ToList();
+    int counter = 0;
+    while (counter <= n / 2)
+    {
+        if (candidates[counter] > 0)
+        {
+            for (int i = counter + candidates[counter]; i < candidates.Count; i += candidates[counter])
+            {
+                candidates[i] = 0;
+            }
+        }
+        ++counter;
+    }
+    return candidates.Where(c => c != 0).ToList();
+}
